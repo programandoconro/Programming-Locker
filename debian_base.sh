1a80d100c22697a65eafd511c261c09b6608cc23
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-08-16 18:43:37 +0100

    Update debian_base.sh

--- debian_base.sh
+++ debian_base.sh
@@ -1,8 +1,4 @@
-
-
 # Docker 
-
-
 
 apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
@@ -10,20 +6,17 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(
 apt-get update -y
 apt install docker-ce -y
 
-
-
 #R
+
 add-apt-repository "deb-src https://cloud.r-project.org/bin/linux/debian $(lsb_release -cs)-cran35/"
 apt instal r-base
 
 #etcher
 
 echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
-
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
-
-sudo apt-get update
-sudo apt-get install balena-etcher-electron
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
+apt-get update
+apt-get install balena-etcher-electron
 
 
 
