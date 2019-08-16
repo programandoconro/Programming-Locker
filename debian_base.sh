89c2b0bbc476781e6fdd08ff3f9f7a9b1b6c1fd2
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-08-16 17:49:30 +0100

    Update and rename debian_data_science to debian_base.sh

---
+++ debian_base.sh
@@ -0,0 +1,32 @@
+#R (sin anaconda)
+
+apt install dirmngr apt-transport-https ca-certificates software-properties-common gnupg2
+
+apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
+add-apt-repository 'deb https://cloud.r-project.org/bin/linux/debian stretch-cran35/'
+
+apt update 
+apt install r-base
+
+# Docker 
+
+apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt-get update
+sudo apt install docker-ce
+
+
+#######################################################################
+
+apt isntall synaptic -y
+#ir a synaptic 
+
+apt install snap snapd -y
+snap install vlc -y
+
+#Latex
+apt-get install texlive-full && texmaker -y
+
+
+
