731b3fc8ba120c4d7a429c9455c9c0d8849d0c3e
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-08-16 17:55:16 +0100

    Update debian_base.sh

--- debian_base.sh
+++ debian_base.sh
@@ -14,7 +14,7 @@ apt install apt-transport-https ca-certificates curl gnupg2 software-properties-
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt-get update -y
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 
 
 #######################################################################
