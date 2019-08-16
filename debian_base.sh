c84448cf8e8e01f3f01a143be2442406e6320d5b
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-08-16 18:35:34 +0100

    Update debian_base.sh

--- debian_base.sh
+++ debian_base.sh
@@ -2,6 +2,8 @@
 
 # Docker 
 
+
+
 apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
@@ -10,6 +12,7 @@ apt install docker-ce -y
 docker run -it hello-world
 
 
-
-
+#R
+add-apt-repository "deb-src https://cloud.r-project.org/bin/linux/debian $(lsb_release -cs)-cran35/"
+apt instal r-base
 
