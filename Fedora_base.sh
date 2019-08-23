ac1b7758885002947784b94c3488dd71747d45db
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-08-23 14:55:11 +0100

    Update Fedora_base.sh

--- Fedora_base.sh
+++ Fedora_base.sh
@@ -1,11 +1,15 @@
+#!bash
+
 yum update -y
 yum upgrade -y
 
-yum install nano youtube-dl elinks wget curl redshift\
-r-base pip-python docker.io vlc nmap terminator\
-transmission gnome-tweaks gimp -y
-#Docker
-dnf -y install dnf-plugins-core
+yum install nano youtube-dl elinks wget curl redshift R pip-python nmap terminator transmission gnome-tweaks gimp -y
+
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+  dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E  %fedora).noarch.rpm -y
+  dnf install vlc
+
+dnf -y install dnf-plugins-core 
 
      dnf config-manager \
         --add-repo \
@@ -14,8 +18,3 @@ dnf -y install dnf-plugins-core
 dnf install docker-ce docker-ce-cli containerd.io -y
 
 systemctl start docker
-
-# Etcher
-#wget https://balena.io/etcher/static/etcher-rpm.repo -O /etc/yum.repos.d/etcher-rpm.repo
-
-#yum install -y balena-etcher-electron
