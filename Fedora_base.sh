a4d492db9fa4be9bb5f40e7d89972640afb35888
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-08-23 15:06:52 +0100

    Update Fedora_base.sh

--- Fedora_base.sh
+++ Fedora_base.sh
@@ -3,11 +3,11 @@
 yum update -y
 yum upgrade -y
 
-yum install nano youtube-dl elinks wget curl redshift R pip-python nmap terminator transmission gnome-tweaks gimp -y
+dnf install git nano youtube-dl elinks wget curl redshift R pip-python nmap terminator transmission gnome-tweaks gimp -y
 
 dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
   dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E  %fedora).noarch.rpm -y
-  dnf install vlc
+  dnf install vlc -y
 
 dnf -y install dnf-plugins-core 
 
