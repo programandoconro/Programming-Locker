d952d4d08635c2ced789c0ec4504fe4d476d3c6e
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-08-31 11:33:31 +0100

    Update Fedora_base.sh

--- Fedora_base.sh
+++ Fedora_base.sh
@@ -1,6 +1,5 @@
 #!bash
 
-
 yum update -y
 yum upgrade -y
 
@@ -34,7 +33,7 @@ tar -xvJf tor-browser-linux64-8.5.4_en-US.tar.xz
 
 #rstudio
 wget https://download1.rstudio.org/desktop/centos7/x86_64/rstudio-1.2.1335-x86_64.rpm
-rpm -i rstudio-1.2.1335-x86_64.rpm -y
+rpm -i rstudio-1.2.1335-x86_64.rpm 
 
 /usr/bin/python2.7 -m pip install --upgrade --user virtualenv
 
@@ -42,6 +41,7 @@ echo 'install.packages(c('keras','ggplot2',
 'caret','e1071','randomForest','MASS',
 'tensorflow','zoo','stringr','dplyr',
 'lubridate','shiny','shinydasboard','neuralnet')) ; 
+library(keras) ; library (tensorflow) 
 install_keras() ; install_tensorflow()' > libs.R && Rscript libs.R
 
 yum update -y
