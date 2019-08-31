98df5c4067d452fa28a258cc58c82edc1108374a
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-08-31 12:53:12 +0100

    Update Fedora_base.sh

--- Fedora_base.sh
+++ Fedora_base.sh
@@ -4,27 +4,24 @@ yum update -y
 yum upgrade -y
 
 # programas preferidos 
-dnf install git nano htop youtube-dl wget elinks wget curl python-pip redshift R macchanger nmap terminator transmission gnome-tweaks gimp okular vim -y
+dnf install git nano htop youtube-dl wget elinks wget curl \
+python-pip redshift R macchanger nmap terminator transmission \
+gnome-tweaks gimp okular vim -y
 
 /usr/bin/python2.7 -m pip install --upgrade --user virtualenv
 
 #vlc 
 dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
   dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E  %fedora).noarch.rpm -y
-  dnf install vlc -y
-
-dnf -y install dnf-plugins-core 
+  dnf install vlc dnf-plugins-core -y 
 
 #Docker 
 
 dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 dnf makecache
-
 dnf install docker-ce -y
-
 systemctl enable docker.service
 systemctl start docker.service
-
 
 #tor 
 mkdir tor
@@ -51,29 +48,14 @@ install.packages(c('keras','ggplot2',
 library(keras) ; library (tensorflow) 
 install_keras() ; install_tensorflow() " > libs.R && Rscript libs.R
 
-
 # SSH
 dnf install -y openssh-server
 systemctl start sshd.service
 sudo systemctl enable sshd.service
 
+# wordpress
+mkdir Wordpress && cd Wordpress
+wget https://public-api.wordpress.com/rest/v1.1/desktop/linux/download
+
 yum update -y
 yum upgrade -y
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
