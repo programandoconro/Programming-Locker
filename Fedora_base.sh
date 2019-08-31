0641a170450d12b29ecd505a0d9ac932e8e395f5
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-08-31 10:54:19 +0100

    Update Fedora_base.sh

--- Fedora_base.sh
+++ Fedora_base.sh
@@ -1,20 +1,66 @@
 #!bash
 
+
 yum update -y
 yum upgrade -y
 
-dnf install git nano youtube-dl elinks wget curl redshift R python-pip nmap terminator transmission gnome-tweaks gimp -y
+dnf install git nano youtube-dl wget elinks wget curl python-pip redshift R macchanger nmap terminator transmission gnome-tweaks gimp okular vim -y
 
+/usr/bin/python2.7 -m pip install --upgrade --user virtualenv
+
+#vlc 
 dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
   dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E  %fedora).noarch.rpm -y
   dnf install vlc -y
 
 dnf -y install dnf-plugins-core 
 
-     dnf config-manager \
-        --add-repo \
-        https://download.docker.com/linux/fedora/docker-ce.repo
+#Docker 
 
-dnf install docker-ce docker-ce-cli containerd.io -y
+dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+dnf makecache
 
-systemctl start docker
+dnf install docker-ce -y
+
+systemctl enable docker.service
+systemctl start docker.service
+
+
+#tor 
+mkdir tor
+cd tor
+wget https://www.torproject.org/dist/torbrowser/8.5.4/tor-browser-linux64-8.5.4_en-US.tar.xz
+tar -xvJf tor-browser-linux64-8.5.4_en-US.tar.xz
+
+#rstudio
+wget https://download1.rstudio.org/desktop/centos7/x86_64/rstudio-1.2.1335-x86_64.rpm
+rpm -i rstudio-1.2.1335-x86_64.rpm -y
+
+/usr/bin/python2.7 -m pip install --upgrade --user virtualenv
+
+echo 'install.packages(c(
+'keras',ggplot2','caret','1071',
+'randomForest','MASS','tensorflow',
+'zoo','stringr','dplyr','lubridate',
+'shiny','shinydasboard','neuralnet')) ; install_keras() ; install_tensorflow() > libs.R && Rscript libs.R
+
+yum update -y
+yum upgrade -y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
