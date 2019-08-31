e257f80953fd6ecb1e87484229455348ceecf972
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-08-31 14:25:50 +0100

    Update Fedora_base.sh

--- Fedora_base.sh
+++ Fedora_base.sh
@@ -3,7 +3,8 @@
 yum update -y
 yum upgrade -y
 
-# programas preferidos 
+# programas base
+
 dnf install git nano htop youtube-dl wget elinks wget curl \
 python-pip redshift R macchanger nmap terminator transmission \
 gnome-tweaks gimp okular vim -y
@@ -11,6 +12,7 @@ gnome-tweaks gimp okular vim -y
 /usr/bin/python2.7 -m pip install --upgrade --user virtualenv
 
 #vlc 
+
 dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
   dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E  %fedora).noarch.rpm -y
   dnf install vlc dnf-plugins-core -y 
@@ -23,16 +25,21 @@ dnf install docker-ce -y
 systemctl enable docker.service
 systemctl start docker.service
 
-#tor 
+# tor 
+
 mkdir tor
 cd tor
 wget https://www.torproject.org/dist/torbrowser/8.5.4/tor-browser-linux64-8.5.4_en-US.tar.xz
 tar -xvJf tor-browser-linux64-8.5.4_en-US.tar.xz
 
-cd && mkdir Rstudio && cd Rstudio
 #rstudio
-wget https://download1.rstudio.org/desktop/centos7/x86_64/rstudio-1.2.1335-x86_64.rpm
-#rpm -i rstudio-1.2.1335-x86_64.rpm 
+
+cd && mkdir Rstudio && cd Rstudio
+wget https://download1.rstudio.org/desktop/centos7/x86_64/rstudio-1.2.1335-x86_64.rpm 
+dnf install compat-openssl10.x86_6
+sudo dnf localinstall rstudio-1.2.1335-x86_64.rpm 
+
+# librerias fundamentales
 
 /usr/bin/python2.7 -m pip install --upgrade --user virtualenv
 
@@ -49,11 +56,12 @@ library(keras) ; library (tensorflow)
 install_keras() ; install_tensorflow() " > libs.R && Rscript libs.R
 
 # SSH
+
 dnf install -y openssh-server
 systemctl start sshd.service
 sudo systemctl enable sshd.service
 
-# wordpress
+# Wordpress
 mkdir Wordpress && cd Wordpress
 wget https://public-api.wordpress.com/rest/v1.1/desktop/linux/download
 
@@ -63,6 +71,7 @@ wget https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh
 bash Anaconda3-2019.07-Linux-x86_64.sh
 source ~/.bashrc
 
+# Actualizar
 
 yum update -y
 yum upgrade -y
