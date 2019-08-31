8c12cf7b14f059daf75a634a3439bc94107a685e
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-08-31 11:48:11 +0100

    Update Fedora_base.sh

--- Fedora_base.sh
+++ Fedora_base.sh
@@ -37,12 +37,17 @@ rpm -i rstudio-1.2.1335-x86_64.rpm
 
 /usr/bin/python2.7 -m pip install --upgrade --user virtualenv
 
-echo 'install.packages(c('keras','ggplot2',
+echo " local({
+  r <- getOption('repos')
+  r['CRAN'] <- 'http://cran.cnr.berkeley.edu/'
+  options(repos = r)
+}) ;
+install.packages(c('keras','ggplot2',
 'caret','e1071','randomForest','MASS',
 'tensorflow','zoo','stringr','dplyr',
 'lubridate','shiny','shinydasboard','neuralnet')) ; 
 library(keras) ; library (tensorflow) 
-install_keras() ; install_tensorflow()' > libs.R && Rscript libs.R
+install_keras() ; install_tensorflow() " > libs.R && Rscript libs.R
 
 yum update -y
 yum upgrade -y
