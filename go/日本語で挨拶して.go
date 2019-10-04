0b6bdfd3f3acab2fd79ad7ae38626431ed28c2a9
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-10-05 00:38:09 +0100

    Update 日本語で挨拶して.go

--- go/日本語で挨拶して.go
+++ go/日本語で挨拶して.go
@@ -10,15 +10,17 @@ func main() {
        	y := time.Now()
         z := y.Hour()
 
-// Si son menos de las de la 7 tarde, imprime "Konichiwa"
+// Si son menos de las de la 7 tarde y más de las 6 de la mañana, imprime "Konichiwa"
 
-        if  z > 19 {
+        if  z < 19 && z > 6 {
                 fmt.Println("こにちわ")
         } else {
 	
-// Imprime Konbanwa si son las 7 o más tarde	
+// Imprime Konbanwa si son más de las 7 de la noche y menos de las 7 de la mañana. 
 	
                 fmt.Println("こんばんわ")
         }
 }
+
+
 
