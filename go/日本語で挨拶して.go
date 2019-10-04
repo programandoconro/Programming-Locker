ef6de78030e58a164238c668473f44a8c73e53d0
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-10-05 00:00:10 +0100

    Update and rename 日本語で挨拶して to 日本語で挨拶して.go
    Say good morning or good night in Japanese depending on local Time

--- go/日本語で挨拶して.go
+++ go/日本語で挨拶して.go
@@ -3,8 +3,6 @@ package main
 import (
         "fmt"
 	"time"  
-
-
 )
  
 func main() {
@@ -12,10 +10,14 @@ func main() {
        	y := time.Now()
         z := y.Hour()
 
+// Si son menos de las de la 7 tarde, imprime "Konichiwa"
 
-        if  z > 17 {
+        if  z > 19 {
                 fmt.Println("こにちわ")
         } else {
+	
+// Imprime Konbanwa si son las 7 o más tarde	
+	
                 fmt.Println("こんばんわ")
         }
 }
