52e7d3e56a3273a024e48152359ea1a6799f74a5
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-10-07 16:34:30 +0100

    Update アプリ.go

--- go/アプリ.go
+++ go/アプリ.go
@@ -1,5 +1,4 @@
-       package main
-
+       
     import (
      "net/http"
       "fmt"
@@ -7,17 +6,24 @@
   
     )
 
+//create a server for program instrucions at localhost:listeningport 
+    
     func main() {
        http.HandleFunc("/", HelloServer)
-       http.ListenAndServe(":8590", nil)
+       http.ListenAndServe(":8593",nil)
     }
 
+//Interact with user in command line terminal
+
     func HelloServer(w http.ResponseWriter, r *http.Request) {
-         fmt.Print( "お名前は何: ")
+        fmt.Print( "お名前は何: ")
         var onamae string    
         fmt.Scanln(&onamae)       
    
         fmt.Print("どうぞよろし湖お願いします"+ onamae + "さん")
+
+    
+//Server response
 
      y := time.Now()
      z := y.Hour()
@@ -25,6 +31,7 @@
      if  z < 19 && z > 6 {
 
            fmt.Fprintf(w, "こにちわ", r.URL.Path[1:])
+           fmt.Fprintf(w, "お元気ですか, This are the instructions to use Learning Japanese go program:", r.URL.Path[$
     } else {
 
             fmt.Fprintf(w, "こんばんわ", r.URL.Path[1:])
