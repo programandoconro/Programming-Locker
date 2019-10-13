a1c4f5e584655aa82bf4685339a7d5e29d627960
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-10-13 17:06:06 +0100

    Create Comandos Docker.sh

---
+++ Comandos Docker.sh
@@ -0,0 +1,25 @@
+docker ps
+docker images
+docker rm *ID*
+docker stop *ID*
+systemctl stop docker
+systemctl disable docker 
+systemctl enable docker
+systemctl start docker
+systemctl status docker
+docker run -it ubuntu
+docker-compose up
+docker build . -t myimage
+
+
+# Comandos en Dockerfile
+
+FROM
+RUN
+VOLUME
+COPY
+ADD
+WORKDIR
+EXPOSE
+ENV
+CMD
