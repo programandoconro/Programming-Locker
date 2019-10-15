29d60318d19eb83846eb9a2cf38d25f2bfaa939a
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-10-15 18:21:24 +0100

    Create MySQLexamples.sql

---
+++ LAMP/MySQLexamples.sql
@@ -0,0 +1,9 @@
+select round(sum(LAT_N),2), round( sum(LONG_W),2) from STATION
+
+SELECT ROUND (MAX(LAT_N), 4) FROM STATION WHERE LAT_N < 137.2345
+
+SELECT ROUND (LONG_W, 4) FROM STATION WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATION WHERE LAT_N < 137.2345);
+
+SELECT ROUND (MIN(LAT_N),4)FROM STATION WHERE LAT_N > 38.7780;
+
+select round(abs(min(lat_n)- max(lat_n)) + abs(min(long_w)- max(long_w)),4) from  station;
