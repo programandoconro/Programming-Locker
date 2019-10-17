83cfff5f04612ae2a958a1cea1d3e4d75959bebb
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-10-17 12:51:19 +0100

    Update basic_software.sh

--- Raspberry/basic_software.sh
+++ Raspberry/basic_software.sh
@@ -1,8 +1,6 @@
-sudo apt install mariadb-server apache2 php /
+sudo apt install mariadb-server apache2 php \
 r-base nmap cmatrix bastet moon-buggy ninvaders nsnake \ 
-pacman4console neofetch figlet htop -y
-
-echo "install.packages('ggplot2')" > requirements.R && Rscript requirements.R
+pacman4console neofetch figlet htop git -y
 
 # Install Golang on Raspberry Pi Zero
 url=`curl https://golang.org/dl/ | grep armv6l | sort --version-sort | tail -1 | grep -o -E https://dl.google.com/go/go[0-9]+\.[0-9]+((\.[0-9]+)?).linux-armv6l.tar.gz` 
@@ -13,3 +11,5 @@ export GOPATH=$HOME/go
 export PATH=/usr/local/go/bin:$PATH:$GOPATH/bin
 EOF
 source ~/.bashrc
+
+echo "install.packages('ggplot2')" > requirements.R && Rscript requirements.R
