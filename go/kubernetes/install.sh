b6a31fd885bd0f349b28ffe66aa1d64bcb1989c3
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-10-17 21:05:48 +0100

    Update install.sh

--- go/kubernetes/install.sh
+++ go/kubernetes/install.sh
@@ -1,3 +1,10 @@
+#33333333333333333333333333333333333333333333333333333333333333333333333333333333333
+# KIND: Si tienes Docker y Go. Simplemente ejutar:
+
+GO111MODULE="on" go get sigs.k8s.io/kind@v0.5.1 && kind create cluster
+
+#####################################################################################3
+# Si tienes solo Go
 sudo -i
 mkdir -p $GOPATH/src/k8s.io
 cd $GOPATH/src/k8s.io
@@ -5,5 +12,12 @@ git clone https://github.com/kubernetes/kubernetes
 cd kubernetes
 make
 
+##################################################3
+
 # Kubernetes for Docker users.
 #https://kubernetes.io/docs/reference/kubectl/docker-cli-to-kubectl/
+
+############ BINARIOS
+curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/v0.5.1/kind-$(uname)-amd64
+chmod +x ./kind
+mv ./kind /some-dir-in-your-PATH/kind
