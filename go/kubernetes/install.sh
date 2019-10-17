b1f883825d4e12332f2851ac258f6f0d810f64be
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-10-17 19:38:49 +0100

    Update install.sh

--- go/kubernetes/install.sh
+++ go/kubernetes/install.sh
@@ -4,3 +4,6 @@ cd $GOPATH/src/k8s.io
 git clone https://github.com/kubernetes/kubernetes
 cd kubernetes
 make
+
+# Kubernetes for Docker users.
+#https://kubernetes.io/docs/reference/kubectl/docker-cli-to-kubectl/
