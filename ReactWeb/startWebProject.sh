e5fac90aa14f7f7f5855b372944c7d2e37dfc902
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-11-04 15:41:53 +0000

    Update startWebProject.sh

--- ReactWeb/startWebProject.sh
+++ ReactWeb/startWebProject.sh
@@ -1,7 +1,8 @@
+apt install npm nodejs -y
+
 npm init -y
 
 # dependencias:
-
 npm i -S react react-dom
 npm i -D webpack webpack-dev-server babel-cli babel-preset-es2015 babel-preset-stage-2 babel-loader
 
