b5b29dbe37e9f2c702eb3dfaee3076989080b66e
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-11-05 16:31:39 +0000

    Update startWebProject.sh

--- ReactWeb/startWebProject.sh
+++ ReactWeb/startWebProject.sh
@@ -6,3 +6,6 @@ npm init -y
 npm i -S react react-dom
 npm i -D webpack webpack-dev-server babel-cli babel-preset-es2015 babel-preset-stage-2 babel-loader
 
+# o simplemente 
+sudo npm install -g create-react-app
+create-react-app example-app
