d2be1431b2c8ebb14f2bd5245bc491963486d73f
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-11-23 15:05:48 +0000

    Create index.js

---
+++ JavaScript/index.js
@@ -0,0 +1,35 @@
+console.log('hola amigo');
+
+const test = () => console.log('Holaaa!! ');
+test();
+
+const resolveAfter10Seconds=()=> {
+  return new Promise(saludos => {
+    setTimeout(() => {
+      saludos('Saludos al mundo, extraterrestre!');
+    }, 10000);
+  });
+};
+
+const resolveAfter5Seconds=()=> {
+  return new Promise(saludos => {
+    setTimeout(() => {
+      saludos('Saludos al mundo, terrícola!');
+    }, 5000);
+  });
+};
+
+async function  asyncCallExtraterrestre() {
+  console.log('calling');
+  const result = await resolveAfter10Seconds();
+  console.log(result);
+};
+
+async function  asyncCallTerricola() {
+  console.log('esperando');
+  const result = await resolveAfter5Seconds();
+  console.log(result);
+};
+
+asyncCallExtraterrestre();
+asyncCallTerricola();
