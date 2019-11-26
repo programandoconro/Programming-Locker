103c2e4fc11db4719cd274ec50d9bd023a6b71bc
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-11-26 22:50:31 +0000

    Create map.js

---
+++ JavaScript/nodejs/map.js
@@ -0,0 +1,6 @@
+const data = ['peón', 'caballo', 'alfil', 'torre','dama','rey'] ;
+const agregaAlgo = (algo) => {console.log('hola ' + algo)}
+
+for (i=0; i<6; i++){console.log('hello' + data[i])}
+
+data.map(agregaAlgo)
