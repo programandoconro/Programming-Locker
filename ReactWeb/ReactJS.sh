740c00c8bb4d27af70eb28bc8e8eae03965ea84e
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-12-13 00:09:20 +0000

    Update ReactJS.sh

--- ReactWeb/ReactJS.sh
+++ ReactWeb/ReactJS.sh
@@ -32,3 +32,10 @@ git init
 git remote add origin https://github.com/gitname/react-gh-pages.git
 
 npm run deploy
+
+# *** Installation in Raspberry Pi zero W ***
+curl -o node-v9.7.1-linux-armv6l.tar.gz https://nodejs.org/dist/v9.7.1/node-v9.7.1-linux-armv6l.tar.gz
+tar -xzf node-v9.7.1-linux-armv6l.tar.gz
+sudo cp -r node-v9.7.1-linux-armv6l/* /usr/local/
+sudo npm install -g create-react-app
+
