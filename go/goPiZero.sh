867fce54e8938c257fcaed06273d9f2b5b3322a1
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-01-11 18:22:59 +0000

    Update goPiZero.sh

--- go/goPiZero.sh
+++ go/goPiZero.sh
@@ -1,10 +1,5 @@
 #!bash
 # Install Golang on Raspberry Pi Zero
-url=`curl https://golang.org/dl/ | grep armv6l | sort --version-sort | tail -1 | grep -o -E https://dl.google.com/go/go[0-9]+\.[0-9]+((\.[0-9]+)?).linux-armv6l.tar.gz` 
-wget ${url}
-sudo tar -C /usr/local -xvf `echo ${url} | cut -d '/' -f5`
-cat >> ~/.bashrc << 'EOF'
-export GOPATH=$HOME/go
-export PATH=/usr/local/go/bin:$PATH:$GOPATH/bin
-EOF
-source ~/.bashrc
+wget https://storage.googleapis.com/golang/go1.9.linux-armv6l.tar.gz
+sudo tar -C /usr/local -xzf go1.9.linux-armv6l.tar.gz
+export PATH=$PATH:/usr/local/go/bin # put into ~/.profile
