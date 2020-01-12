e4a8e079e28929cc222fb79adda1b82f78e99be1
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-01-12 09:16:28 -0500

    Update debian_base.sh

--- Linux/debian_base.sh
+++ Linux/debian_base.sh
@@ -1,3 +1,5 @@
+## Debian 10
+
 # Docker 
 
 apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
@@ -14,9 +16,15 @@ apt install r-base -y
 #etcher
 
 echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
-
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
 apt-get update
 apt-get install balena-etcher-electron
+
+# Skype 
+
+wget https://repo.skype.com/latest/skypeforlinux-64.deb
+sudo dpkg -i skypeforlinux-64.deb
+sudo apt-get install -f
 
 
 
