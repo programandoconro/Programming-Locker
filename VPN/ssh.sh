035312bc2f59f7fe039dfbc7ae83588279ec039f
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-01-21 23:31:27 +0000

    Update ssh.sh

--- VPN/ssh.sh
+++ VPN/ssh.sh
@@ -4,6 +4,7 @@
 apt-get install openssh-server
 systemctl enable ssh 
 systemctl start ssh
+systemclt status ssh
 
 # Agregar llaves para no usar contraseñas.
 ssh-keygen
@@ -19,13 +20,15 @@ scp -r /dir/ user@remote:~/Destino/
 
 ssh user@remote.local ls
 
-# por flojera podemos escribrir un script bash para conectar rapidamente.
-
-#!bash
-ssh user@remote.local
-
 # Para conectar fuera de la red local necesitamos la IP pública del router y haber abierto un túnel para un servidor local
 
 ssh user@IP 
 
-# Si el túnel esta abierto tendremos acceso a los equipos conectados en la red. 
+# Firewall
+ufw allow 22/tcp # o ufw allow 2222/tcp
+ufw allow from 202.54.1.1 to any port 22
+ufw limit ssh
+ufw status
+
+systemctl stop ssh
+systemclt disable ssh
