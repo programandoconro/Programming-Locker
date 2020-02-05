6e4f491dd416911b0fecbbad56c7535b0b3cad9b
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-02-05 22:05:35 +0000

    Update install.sh

--- ReactNative/install.sh
+++ ReactNative/install.sh
@@ -7,6 +7,8 @@ lsusb
 
 echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="04e8", MODE="0666", GROUP="plugdev"' | sudo tee /etc/udev/rules.d/51-android-usb.rules
 
+apt install adb -y
+
 adb devices
 
 #Habilita la conexión en tu teléfono.
