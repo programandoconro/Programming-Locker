4e67231b912757e7c7a5fd9e4fc8392b4af16459
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-02-14 09:36:33 +0000

    Update Fedora_base.sh

--- Linux/Fedora_base.sh
+++ Linux/Fedora_base.sh
@@ -11,15 +11,14 @@ dnf install git nano htop youtube-dl wget elinks wget curl \
 python-pip R macchanger nmap terminator transmission \
 gnome-tweaks gimp okular vim -y
 
-/usr/bin/python2.7 -m pip install --upgrade --user virtualenv
+# Snap
+yum install snapd
+ln -s /var/lib/snapd/snap /snap/
 
-#vlc 
+# Visual Studio Code
+snap install code --classic
 
-dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
-  dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E  %fedora).noarch.rpm -y
-  dnf install vlc dnf-plugins-core -y 
-
-# tor 
+# Tor 
 
 mkdir tor
 cd tor
@@ -34,9 +33,7 @@ wget https://download1.rstudio.org/desktop/centos7/x86_64/rstudio-1.2.1335-x86_6
 dnf install compat-openssl10.x86_6 -y
 dnf localinstall rstudio-1.2.1335-x86_64.rpm -y 
 
-# librerias fundamentales
-
-/usr/bin/python2.7 -m pip install --upgrade --user virtualenv
+# librerias R fundamentales
 
 echo " local({
   r <- getOption('repos')
@@ -49,12 +46,6 @@ install.packages(c('keras','ggplot2',
 'lubridate','shiny','shinydasboard','neuralnet')) ; 
 library(keras) ; library (tensorflow) ;
 install_keras() ; install_tensorflow() " > libs.R && sudo Rscript libs.R
-
-# SSH
-
-dnf install -y openssh-server
-systemctl start sshd.service
-sudo systemctl enable sshd.service
 
 # Wordpress
 
@@ -70,29 +61,10 @@ wget https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh
 bash Anaconda3-2019.07-Linux-x86_64.sh -y
 source ~/.bashrc -y
 
-#etcher
-echo “deb https://deb.etcher.io stable etcher” | sudo tee /etc/apt/sources.list.d/balena-etcher.list
-apt-key adv –keyserver keyserver.ubuntu.com –recv-keys 379CE192D401AB61
-apt-get update
-apt-get install balena-etcher-electron
+systemctl stop cups
+systemctl disable cups
 
-# VisualStudio
-
-vim /etc/yum.repos.d/vscode.repo
- 
- ###################### add to file ###################
- [vscode]
-name=Visual Studio Code
-baseurl=https://packages.microsoft.com/yumrepos/vscode
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.microsoft.com/keys/microsoft.asc
-########################################################
-
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo dnf install code
-
-# Actualizar
+# Actualizar y reiniciar
 
 yum update -y
-yum upgrade -y
+reboot
