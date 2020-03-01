191cc0de5b3ab1482dcd9e3711d1dbc769c9daf2
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-03-01 00:07:37 +0000

    Create Docker_images.sh

---
+++ Docker/Docker_images.sh
@@ -0,0 +1,29 @@
+# RSTUDIO
+# una instancia
+docker run --rm -p 8787:8787 -e PASSWORD=yourpasswordhere rocker/rstudio
+
+# Servidor
+docker run -d -p 8787:8787  --restart always -v $HOME:/home/'whoami' -e PASSWORD=****** rocker/rstudio
+
+# Ve a 0.0.0.0:8787 o localhost:8787
+#nombre: rstudio
+#clave: ******
+
+# JUPYTER LAB SPARK
+docker run --rm -p 8888:8888 -p 4040:4040 -e JUPYTER_ENABLE_LAB=yes -v ~:/home/ro/jupyter jupyter/all-spark-notebook
+
+# JUPYTER SPARK
+docker run -it --rm -p 8888:8888 -p 4040:4040 -v ~:/home/ro/python jupyter/all-spark-notebook
+
+# KNOWN OS
+docker run -it ubuntu 
+docker run -it centos
+docker run -it debian
+docker run -it alpine
+
+# MAKE DOCKERFILE 
+# FROM
+# RUN
+# COPY
+
+docker build .
