11d0f574fbe6595e80ea87aa3e36df55d370970e
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-03-02 14:42:08 +0000

    Update Docker_images.sh

--- Docker/Docker_images.sh
+++ Docker/Docker_images.sh
@@ -1,17 +1,16 @@
 # RSTUDIO
 # una instancia
 docker run --rm -p 8787:8787 -e PASSWORD=yourpasswordhere rocker/rstudio
-
 # Servidor
 docker run -d -p 8787:8787  --restart always -v $HOME:/home/'whoami' -e PASSWORD=****** rocker/rstudio
-
+# o con podman (Fedora 31)
+podman run -d -p 8787:8787  --restart always -v $HOME:/home/'whoami' -e PASSWORD=****** rocker/rstudio
 # Ve a 0.0.0.0:8787 o localhost:8787
 #nombre: rstudio
 #clave: ******
 
 # JUPYTER LAB SPARK
 docker run --rm -p 8888:8888 -p 4040:4040 -e JUPYTER_ENABLE_LAB=yes -v ~:/home/ro/jupyter jupyter/all-spark-notebook
-
 # JUPYTER SPARK
 docker run -it --rm -p 8888:8888 -p 4040:4040 -v ~:/home/ro/python jupyter/all-spark-notebook
 
@@ -26,4 +25,5 @@ docker run -it alpine
 # RUN
 # COPY
 
-docker build .
+docker build . -t mydocker
+docker run -it mydocker
