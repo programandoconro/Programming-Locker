a6b93f8eae5715707f2c395efc136c3cc5bacde9
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-03-10 10:31:32 +0000

    Update Docker_images.sh

--- Docker/Docker_images.sh
+++ Docker/Docker_images.sh
@@ -1,18 +1,24 @@
 # RSTUDIO
-# una instancia
+# One instance
 docker run --rm -p 8787:8787 -e PASSWORD=yourpasswordhere rocker/rstudio
-# Servidor
+# Server
 docker run -d -p 8787:8787  --restart always -v $HOME:/home/'whoami' -e PASSWORD=****** rocker/rstudio
-# o con podman (Fedora 31)
+# or with podman (Fedora 31)
 podman run -d -p 8787:8787  --restart always -v $HOME:/home/'whoami' -e PASSWORD=****** rocker/rstudio
-# Ve a 0.0.0.0:8787 o localhost:8787
-#nombre: rstudio
-#clave: ******
+# Go to 0.0.0.0:8787 or localhost:8787
+#name: rstudio
+#password: ******
 
 # JUPYTER LAB SPARK
 docker run --rm -p 8888:8888 -p 4040:4040 -e JUPYTER_ENABLE_LAB=yes -v ~:/home/ro/jupyter jupyter/all-spark-notebook
 # JUPYTER SPARK
 docker run -it --rm -p 8888:8888 -p 4040:4040 -v ~:/home/ro/python jupyter/all-spark-notebook
+
+# Anaconda3 
+#docker
+docker run -i -t continuumio/anaconda3 /bin/bash 
+#podman(FEDORA)
+podman run -i -t continuumio/anaconda3 /bin/bash
 
 # KNOWN OS
 docker run -it ubuntu 
@@ -20,10 +26,16 @@ docker run -it centos
 docker run -it debian
 docker run -it alpine
 
-# MAKE DOCKERFILE 
+#COMMANDS FOR DOCKERFILE 
 # FROM
 # RUN
 # COPY
+# ADD
+# CMD
+# ENTRYPOINT 
+# EXPOSE
+# ENV
 
+#then run this command in the directory where Dockerfile exists. 
 docker build . -t mydocker
 docker run -it mydocker
