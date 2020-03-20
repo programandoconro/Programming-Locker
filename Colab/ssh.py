1bfd3249fe0c14a34a9bd00b4e5ff945ae389a2c
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-03-20 23:22:10 +0000

    Update ssh.py

--- Colab/ssh.py
+++ Colab/ssh.py
@@ -3,3 +3,8 @@
 !pip install git+https://github.com/demotomohiro/remocolab.git
 import remocolab
 remocolab.setupSSHD()
+
+# ssh -o UserKnownHostsFile=/dev/null -o VisualHostKey=yes -p 14533 root@0.tcp.eu.ngrok.io
+# ssh-keygen
+# ssh-copy-id -i ~/.ssh/id_rsa.pub root@0.tcp.eu.ngrok.io -p 14533
+# ssh-add
