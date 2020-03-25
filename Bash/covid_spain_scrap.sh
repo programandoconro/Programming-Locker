a366c2cf49ed1e669dbc18a3c611618b86aec822
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-03-25 23:09:33 +0000

    Update covid_spain_scrap.sh

--- Bash/covid_spain_scrap.sh
+++ Bash/covid_spain_scrap.sh
@@ -1,5 +1,5 @@
 for i in 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54
 do
-sudo wget -A pdf -m -p -E -k -K -np "https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov-China/documentos/Actualizacion_"$i"_COVID-19.pdf" 
+sudo wget "https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov-China/documentos/Actualizacion_"$i"_COVID-19.pdf" 
 echo "File: $i"
 done  
