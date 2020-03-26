cb8c1c23921d1f5a4f21a4fb18cc776d2b7335fb
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-03-26 16:59:34 +0000

    Update mining.py

--- python/pdfReader/mining.py
+++ python/pdfReader/mining.py
@@ -1,9 +1,6 @@
 import os
+import coronaCanarias
 
-os.system('grep Canarias -A 2 noisy_data.txt > results.txt')
-
-os.system('cat results.txt')
+os.system('grep Canarias -A 2 noisy_data.txt > results.txt && cat results.txt' )
 
 file = open('results.txt').read()
-
-file
