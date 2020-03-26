7c8beef53e70d6018479ce1e09be9f5d4cc2bf92
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-03-26 17:53:31 +0000

    Add files via upload

--- python/pdfReader/CoronaCanarias.py
+++ python/pdfReader/CoronaCanarias.py
@@ -1,34 +1,32 @@
 import PyPDF2
 import os
 
-os.system('sh get_data.sh')
-
-files = []
-project_path = "/home/roro/COVID_canarias/"
-for i in os.listdir(project_path):
-    if i.endswith('.pdf'):
-        files.append(i)
-        files.sort()
-
-pages = []
-noysy_data = []
-for i in files:
-    pdfFileObj = open(i, 'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    n_pag = pdfReader.numPages   	
-    for j in range(0, n_pag):
-        pageObj = pdfReader.getPage(j)
-        pages.append(pageObj.extractText())
-        noysy_data.append(pages)
-    	    
-os.system("touch noisy_data.txt")
-txt = open("noisy_data.txt", "w")
-
-data = ''
-for i in range(0,len(noysy_data)):      
-	r = data.join(noysy_data[i])
-
-txt.write(r)        
-txt.close()
+# os.system('sh get_data.sh')
 
 
+def pdfs_to_txt(path):
+
+    files = []
+    for i in os.listdir(path):
+        if i.endswith('.pdf'):
+            files.append(i)
+            files.sort()
+    pages = []
+    noysy_data = []
+
+    for i in files:
+        pdfFileObj = open(i, 'rb')
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+        n_pag = pdfReader.numPages
+        for j in range(0, n_pag):
+            pageObj = pdfReader.getPage(j)
+            pages.append(pageObj.extractText())
+            noysy_data.append(pages)
+
+    os.system("touch noisy_data.txt")
+    txt = open("noisy_data.txt", "w")
+    data = ''
+    for i in range(0, len(noysy_data)):
+        r = data.join(noysy_data[i])
+    txt.write(r)
+    txt.close()
--- python/pdfReader/mining.py
+++ python/pdfReader/mining.py
@@ -1,6 +1,7 @@
 import os
-import coronaCanarias
+from CoronaCanarias import pdfs_to_txt
 
-os.system('grep Canarias -A 2 noisy_data.txt > results.txt && cat results.txt' )
-
+pdfs_to_txt(path="/home/roro/COVID_canarias/")
+os.system('grep Canarias -A 2 noisy_data.txt > results.txt && cat results.txt')
 file = open('results.txt').read()
+
