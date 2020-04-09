18463d9a9cf13d465c146b90ebcb1f7856600e71
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-04-09 02:28:49 +0100

    Update MySQLexamples.sql

--- LAMP/MySQLexamples.sql
+++ LAMP/MySQLexamples.sql
@@ -2,17 +2,16 @@
 
 create database test;
 show databases;
-use test
-create table data1 (Id int);
+use test;
+create table data (Id int, Contagiados int, Recuperados int, Muertos int, Fecha );
+insert into data values (0,0,0,0);
+alter table data add (Fecha date);
+select * from data; 
 
-#####################
+#### Examples #####################
 
-select round(sum(LAT_N),2), round( sum(LONG_W),2) from STATION
-
-SELECT ROUND (MAX(LAT_N), 4) FROM STATION WHERE LAT_N < 137.2345
-
+select round(sum(LAT_N),2), round( sum(LONG_W),2) from STATION;
+SELECT ROUND (MAX(LAT_N), 4) FROM STATION WHERE LAT_N < 137.2345;
 SELECT ROUND (LONG_W, 4) FROM STATION WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATION WHERE LAT_N < 137.2345);
-
 SELECT ROUND (MIN(LAT_N),4)FROM STATION WHERE LAT_N > 38.7780;
-
 select round(abs(min(lat_n)- max(lat_n)) + abs(min(long_w)- max(long_w)),4) from  station;
