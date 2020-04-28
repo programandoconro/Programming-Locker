df498426579b437e4de4cf06e03ad64296b81357
Ro <50117686+programandoconro@users.noreply.github.com>
2020-04-28 11:35:42 +0100

    Update api.js

--- JavaScript/api.js
+++ JavaScript/api.js
@@ -1,4 +1,27 @@
+// Get json data from API, for example from: https://api.github.com/users/programandoconro
+
+// Example 1
+
+const callAPI = async () => {
+  const res = await fetch(" https://api.github.com/users/programandoconro");
+  const data = await res.json();
+  console.log(data);
+};
+
+// Example 2
 const xhr = new XMLHttpRequest()
 xhr.open('GET','https://api.github.com/users/programandoconro',true)
 xhr.send()
 xhr.responseText
+
+// Example 3
+const callAPI3 = () => {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://api.github.com/users/programandoconro");
+  xhr.responseType = "json";
+  xhr.send();
+  xhr.onload = function () {
+    console.log(xhr.response);
+  };
+};
+callAPI3();
