38b16368f62980e06f08df389c40a958b2480a51
Ro <50117686+programandoconro@users.noreply.github.com>
2020-05-07 16:54:04 +0100

    Create install_pkgs.R

---
+++ R/Dependencies/install_pkgs.R
@@ -0,0 +1,30 @@
+# Dependencias:
+if(FALSE) {
+
+'* deb: libssl-dev (Debian, Ubuntu, etc)
+ * rpm: openssl-devel (Fedora, CentOS, RHEL)
+ * csw: libssl_dev (Solaris)
+ * brew: openssl@1.1 (Mac OSX)
+
+ * deb: libcurl4-openssl-dev (Debian, Ubuntu, etc)
+ * rpm: libcurl-devel (Fedora, CentOS, RHEL)
+ * csw: libcurl_dev (Solaris)
+ '
+
+}
+####################################################
+
+requirements <- c('tidyverse','randomForest','ipred','tm',
+'rpart', 'xgboost', 'dplyr', 'twitteR','stringr','lubridate',
+'dplyr','ggthemes','wordcloud','data.table','SnowballC' )
+
+pack <- requirements[!requirements %in% installed.packages()[,"Package"]]
+if(length(pack)) {install.packages(pack)} else {
+    print('Todas las dependencias han sido instaladas')
+}
+
+for (i in requirements){ 
+  library(i, character.only = TRUE)
+  print(c(i,"LOADED"))
+ 
+}
