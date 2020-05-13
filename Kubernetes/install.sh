a77c357912e6e4ba7de9bf4a6fbc8ad4aa3a1f14
Ro <50117686+programandoconro@users.noreply.github.com>
2020-05-13 10:48:22 +0100

    Update install.sh

--- Kubernetes/install.sh
+++ Kubernetes/install.sh
@@ -1,13 +1,10 @@
 # Debian 10
 
-apt-get update && sudo apt-get install -y$
-curl -s https://packages.cloud.google.com$
-cat <<EOF | sudo tee /etc/apt/sources.lis$
-deb https://apt.kubernetes.io/ kubernetes$
+apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update -y
 apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
-
-virsh net-autostart default
-virsh net-start default
