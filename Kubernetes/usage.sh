c8bf3e59ca2bd3083b651b6d2dbcbb77e8ede1c5
Ro <50117686+programandoconro@users.noreply.github.com>
2020-05-16 23:16:08 +0100

    Create usage.sh

---
+++ Kubernetes/usage.sh
@@ -0,0 +1,9 @@
+kubectl cluster-info --context kind-kind
+
+docker run -d --restart=always -e DOMAIN=cluster --name nginx-app -p 808:80 nginx
+kubectl run --image=nginx nginx-app --port=808 --env="DOMAIN=cluster"
+kubectl expose pod nginx-apps --port=808 --name=nginx-http
+
+kubeadm init --v=11
+worker# 
+kubeadm join <arguments-returned-from-init>
