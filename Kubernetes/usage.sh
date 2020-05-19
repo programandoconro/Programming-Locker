3912c945215936feea58500eac29cb3bec6f1589
Ro <50117686+programandoconro@users.noreply.github.com>
2020-05-19 21:11:05 +0100

    Update usage.sh

--- Kubernetes/usage.sh
+++ Kubernetes/usage.sh
@@ -2,7 +2,7 @@ kubectl cluster-info --context kind-kind
 
 docker run -d --restart=always -e DOMAIN=cluster --name nginx-app -p 808:80 nginx
 kubectl run --image=nginx nginx-app --port=808 --env="DOMAIN=cluster"
-kubectl expose pod nginx-apps --port=808 --name=nginx-http
+kubectl expose pod nginx-app --port=808 --name=nginx-http
 
 kubeadm init --v=11
 worker# 
