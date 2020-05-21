495eb6e5246376eff5cd0099a1d6813f0757ea4d
Ro <50117686+programandoconro@users.noreply.github.com>
2020-05-21 01:04:19 +0100

    Update commands.sh

--- Kubernetes/Cluster/commands.sh
+++ Kubernetes/Cluster/commands.sh
@@ -1,7 +1,12 @@
 kubectl config view
+kubectl cluster-info
+
+kubectl describe nodes
+kubectl describe pods
+
 kubectl get pods
 kubectl get nodes
 kubectl get deployments
+kubectl get services
 
 kubectl delete node <name>
-
