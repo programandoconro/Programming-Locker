c4fd00b76fd528043cb5ca86d7ec0306d4e885cd
python <programandoconro@gmail.com>
2020-05-23 00:35:50 +0100

    update

--- Kubernetes/Cluster/commands.sh
+++ Kubernetes/Cluster/commands.sh
@@ -15,3 +15,10 @@ kubectl exec -it <name> -- /bin/bash
 
 sudo cat /etc/rancher/k3s/k3s.yaml
 sudo cat /var/lib/rancher/k3s/server/node-token
+
+# usage with Docker
+
+docker run -d --restart=always -e DOMAIN=cluster --name nginx-app -p 808:80 nginx
+kubectl run --image=nginx nginx-app --port=808 --env="DOMAIN=cluster"
+kubectl expose pod nginx-app --port=808 --name=nginx-http
+
