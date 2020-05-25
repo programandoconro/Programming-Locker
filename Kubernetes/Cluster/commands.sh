df6a9aa754ef808f954b193cfc02922dc5adcf97
Ro <50117686+programandoconro@users.noreply.github.com>
2020-05-25 19:52:14 +0100

    Update commands.sh

--- Kubernetes/Cluster/commands.sh
+++ Kubernetes/Cluster/commands.sh
@@ -1,3 +1,5 @@
+## Kubernetes commands:
+
 # General info:
 kubectl config view
 kubectl cluster-info
@@ -6,15 +8,19 @@ kubectl get pods
 kubectl get nodes
 kubectl get deployments
 kubectl get services
+kubectl get replicasets
+kubectl get secrets
 
 # Detailed info:
 kubectl describe nodes
 kubectl describe pods
 kubectl describe services
+kubectl describe replicasets
 
 # Delete:
 kubectl delete node <name>
-sudo kubectl delete pods --all
+kubectl delete pods --all
+kubectl delete service <name>
 
 # Credentials: 
 sudo cat /etc/rancher/k3s/k3s.yaml
@@ -25,10 +31,15 @@ docker run -d --restart=always -e DOMAIN=cluster --name nginx-app -p 808:80 ngin
 kubectl run --image=nginx nginx-app --port=808 --env="DOMAIN=cluster"
 kubectl expose pod nginx-app --port=808 --name=nginx-http
 
-# Deployment:
+# Deployment nginx:
 kubectl apply -f https://k8s.io/examples/application/deployment.yaml
 kubectl describe deployment nginx-deployment
 
 # shell access:
 kubectl run my-shell --rm -i --tty --image ubuntu -- bash
 kubectl exec -it <name> -- /bin/bash
+
+
+
+
+
