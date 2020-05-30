a1b302dcd7121386172aca98b6f048047c45d522
Ro <50117686+programandoconro@users.noreply.github.com>
2020-05-30 20:30:41 +0100

    Create app.R

---
+++ Docker/R/shiny/app.R
@@ -0,0 +1,43 @@
+library(shiny)
+
+ui <- fluidPage(
+
+  titlePanel("Hello World Shiny-Server in Docker!"),
+
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+
+      # Input: Slider for the number of bins ----
+      sliderInput(inputId = "bins",
+                  label = "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+
+    ),
+
+    mainPanel(
+    
+      plotOutput(outputId = "distPlot")
+
+    )
+  )
+)
+
+server <- function(input, output) {
+
+  output$distPlot <- renderPlot({
+
+    x    <- faithful$waiting
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+    hist(x, breaks = bins, col = "#75AADB", border = "white",
+         xlab = "Waiting time to next eruption (in mins)",
+         main = "Histogram of waiting times")
+
+    })
+
+}
