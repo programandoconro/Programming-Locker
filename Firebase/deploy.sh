e89522f9a7f3d5fd2a5d94894beb143a6bd0bd10
Ro <50117686+programandoconro@users.noreply.github.com>
2020-06-04 16:28:48 +0100

    Create deploy.sh

---
+++ Firebase/deploy.sh
@@ -0,0 +1,14 @@
+# Deploy a React App in Firebase Cloud service.
+
+ npm i -g firebase-tools
+ 
+ git clone https://github.com/programandoconro/KanjiTrainer
+ cd KanjiTrainer/
+ 
+ npm install
+ npm run build
+ 
+ firebase init 
+ # choose build, Yes and N.
+ 
+ firebase deploy
