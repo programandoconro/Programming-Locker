3fb66f6e21441c50194a0ab9374c3cd4e090fa7b
Ro <50117686+programandoconro@users.noreply.github.com>
2020-06-05 10:48:17 +0100

    Update app.R

--- R/shiny/shinyJS/app.R
+++ R/shiny/shinyJS/app.R
@@ -5,8 +5,8 @@ js_inline_code <- tags$script(HTML('console.log("hello world shiny js")'))
 js_external_code <- includeScript(path ='myscript.js')
 
 ui <- fluidPage(
-js_inline_code,
-js_external_code
+  js_inline_code,
+  js_external_code
 )
 
 server <- function( input, output ){}
