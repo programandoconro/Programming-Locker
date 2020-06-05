e26fb0cafbd7ab1142f148f2ba41ffd6d3d45866
Ro <50117686+programandoconro@users.noreply.github.com>
2020-06-05 11:17:33 +0100

    Update app.R

--- R/shiny/shinyJS/app.R
+++ R/shiny/shinyJS/app.R
@@ -2,13 +2,32 @@ library(shiny)
 
 js_inline_code <- tags$script(HTML('console.log("hello world shiny js")'))
 
-js_external_code <- includeScript(path ='myscript.js')
+js_external_code <- includeScript(path = 'myscript.js')
+
+html_inline_code <- h1('Using HTML, Javascript and CSS in a R Shiny App')
+
+html_external_code <- includeHTML('index.html')
 
 ui <- fluidPage(
   js_inline_code,
-  js_external_code
+  js_external_code,
+  html_inline_code,
+  html_external_code,
+
+  plotOutput('plot1')
 )
 
-server <- function( input, output ){}
+server <- function(input, output) {
+  x <- 1:10
+  y <- 11:20
 
-shinyApp( ui, server ) 
+  output$plot1 <- renderPlot({
+
+    plot(x, y)
+    
+  })
+}
+
+shinyApp(ui, server)
+
+
