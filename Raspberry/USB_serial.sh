7a34b678a34ddfca2cdfe94df6b7b999eaae546f
Ro <50117686+programandoconro@users.noreply.github.com>
2020-06-10 11:54:26 +0100

    Update USB_serial.sh

--- Raspberry/USB_serial.sh
+++ Raspberry/USB_serial.sh
@@ -1,22 +1,30 @@
-sudo -i 
-#montamos y navegamos al boot
 
-nano config.txt 
+# Conectar Raspberry Pi Zero (sin WIFI) a otra Raspberry mediante USB serial
 
-#dtoverlay=dwc2 
-
-nano cmdline.txt 
-
-# modules-load=dwc2,g_ether
-
+# Instalamos Raspbian Buster y agregamos archivo ssh a carpeta boot
 touch ssh
 
-#en host 
-echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+# Editamos un par de archivos en carpeta boot
 
-## 
+echo dtoverlay=dwc2 >> config.txt 
+
+nano cmdline.txt 
+# luego de rootwait agreagar: modules-load=dwc2,g_ether
+
+# Accedemos por ssh 
 
 ssh pi@raspberrypi.local
+
+# Cambiamos password y hostname en raspi-config
+
+sudo raspiconfig
+# Siendo root, modificamos en archivo ip_forward y permitimos el routing por 
+sudo -i 
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+
+# Accedemos a la Raspberry mediante USB serial.
+ssh pi@raspberrypi.local
+ifconfig
 
 
