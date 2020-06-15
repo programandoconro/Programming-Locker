e990564613b8b44cf4c96087cf0d6a2011dd5060
Ro <50117686+programandoconro@users.noreply.github.com>
2020-06-15 13:33:56 +0100

    Create install.sh

---
+++ youtube-dl/install.sh
@@ -0,0 +1,4 @@
+sudo -i
+wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
+chmod a+rx /usr/local/bin/youtube-dl
+youtube-dl https://www.youtube.com/watch?v=2jOsSn7F054 
