a9f3caf85856a06826f3da2e85f2f9976ce6187a
programandoconro <programandoconro@gmail.com>
2020-06-27 19:39:10 +0100

    nginx & i3

---
+++ nginx/www/html/build/precache-manifest.73232e0a471984b34b745f97b4781fc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afbb3337054bc288dcb4a1445c60adc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9bbc73da6d14564239c",
+    "url": "/static/css/main.a932290c.chunk.css"
+  },
+  {
+    "revision": "4d92b26208a5127c7cf8",
+    "url": "/static/js/2.5f5ebbd7.chunk.js"
+  },
+  {
+    "revision": "76f107387487693e85d1563bfa8b2155",
+    "url": "/static/js/2.5f5ebbd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9bbc73da6d14564239c",
+    "url": "/static/js/main.41e91f0e.chunk.js"
+  },
+  {
+    "revision": "bace1e5c250713232b62",
+    "url": "/static/js/runtime-main.88daa14b.js"
+  },
+  {
+    "revision": "dc28b4a98dc7323b0ced8dbbef156a2e",
+    "url": "/static/media/robot.dc28b4a9.jpg"
+  }
+]);
---
+++ nginx/www/nihongopherd.xyz/build/precache-manifest.95d7a808734619d952679879622899a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa94daf188b40a032ace7595bb648a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d7407d63e438eef533a",
+    "url": "/static/css/main.060891f1.chunk.css"
+  },
+  {
+    "revision": "60ae54f1dcf26babcb60",
+    "url": "/static/js/2.5dfecd8d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5dfecd8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d7407d63e438eef533a",
+    "url": "/static/js/main.a6946937.chunk.js"
+  },
+  {
+    "revision": "89e11de2005d7c106302",
+    "url": "/static/js/runtime-main.dd61ae29.js"
+  }
+]);
