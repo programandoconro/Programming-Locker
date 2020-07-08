00b0b666c91470f12e1b3727bb2d6ee976f3d16b
Ro <50117686+programandoconro@users.noreply.github.com>
2020-07-09 00:39:27 +0200

    Update install.sh

--- Java/install.sh
+++ Java/install.sh
@@ -1,4 +1,13 @@
-# Manjaro
+# Latest Java in Arch/Manjaro:
+pacman -S jdk-openjdk
+
+# Install a specific version
 pacman -S openjdk11-src
+
+# Adjust default version
+archlinux-java status
+archlinux-java set java-14-openjdk
+
+# Run Hello-world
 pamac helloworld.java
 java HelloWorld
