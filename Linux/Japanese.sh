c2931c1b644908a3ad3820c7111827bab0c1ae31
Ro <50117686+programandoconro@users.noreply.github.com>
2020-08-27 09:43:07 +0900

    Update Japanese.sh

--- Linux/Japanese.sh
+++ Linux/Japanese.sh
@@ -1,6 +1,6 @@
 #Japanese input and fonts in Manjaro and i3 window-manager.
 
-yay -Sy fcitx-mozc ibus-daemon
+yay -Sy fcitx-mozc ibus-daemon ibus-mozc
 pacman -Sy adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fontsotf-ipafont noto-fonts-emoji
 
 sudo echo 'ja_JP.UTF-8 UTF-8' >> /etc/locale.gen
