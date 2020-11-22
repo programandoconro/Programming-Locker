1351db395dcfbf96d9b446d20f89201b6e7517e2
Ro <50117686+programandoconro@users.noreply.github.com>
2020-11-22 21:32:38 +0900

    Update and rename Arduino/CO2_oled.ino to Arduino/CO2/CO2_oled.ino

--- Arduino/CO2/CO2_oled.ino
+++ Arduino/CO2/CO2_oled.ino
@@ -3,43 +3,57 @@
 
 Adafruit_SSD1306 display(4);
 CCS811 sensor;
-int i = 0;
+int Max[2] = {0,0};
+int Timestamp, i = 0;
 
 void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 
   while (sensor.begin() != 0) {
-    Serial.println("failed to init chip, please check if the chip connection is fine");
+    display.println("failed to init chip, please check if the chip connection is fine");
     delay(1000);
   }
-
+  
   sensor.setMeasCycle(sensor.eCycle_250ms);
 
 }
 
 void loop() {
-  i = i + 1;
-  delay(1000);
-  if (sensor.checkDataReady() == true) {
 
+  delay(1000);
+  i = i + 1; 
+  if (sensor.checkDataReady() == true) {
     display.clearDisplay();
     display.setTextColor(WHITE);
     display.setCursor(1, 1);
     display.print("CO2: ");
     display.print(sensor.getCO2PPM());
-    display.println("ppm.");
+    display.println("ppm");
     display.print("TVOC: ");
     display.print(sensor.getTVOCPPB());
-    display.println("ppb.");
-    display.print("s: ");
-    display.println(i);
-    display.println("ro");
+    display.println("ppb");
+
+    if (sensor.getCO2PPM() > Max[0]){
+      Max[0] = sensor.getCO2PPM();
+      Timestamp = i;
+    }
+
+     if (sensor.getTVOCPPB() > Max[1]){
+      Max[1] = sensor.getTVOCPPB();
+    }
+
+    display.print("Max:");
+    display.print(Max[0]);
+    display.print("ppm,");
+    display.print(Max[1]);
+    display.println("ppb");
+    display.print("Ts:");
+    display.print(Timestamp);
+    display.println("s");
     display.display();
 
   } else {
-    Serial.println("Data is not ready!");
     display.println("Data is not ready!");
-
   }
 
   sensor.writeBaseLine(0x847B);
