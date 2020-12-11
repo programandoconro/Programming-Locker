cc9ef8f92d9aad540d297b9c44cc728a47052913
Ro <50117686+programandoconro@users.noreply.github.com>
2020-12-11 13:43:01 +0900

    Create virtualenv.sh

---
+++ Python/virtualenv.sh
@@ -0,0 +1,14 @@
+# Maneja ambientes virtuales para python
+
+pip install virtualenv
+
+python3 -m venv test
+
+source test/bin/activate
+
+pip install requests
+
+pip freeze > requirements.txt
+
+pip install -f requirements.txt
+
