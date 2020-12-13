d3f1f0cdfd2df097a63a11f60e6f2b0ba3d2e6f3
Ro <50117686+programandoconro@users.noreply.github.com>
2020-12-13 19:59:05 +0900

    Create LoRaSender_DHT.ino

---
+++ Arduino/LoRa/DHT/LoRaSender_DHT.ino
@@ -0,0 +1,121 @@
+
+//Libraries for LoRa
+#include <SPI.h>
+#include <LoRa.h>
+
+//Libraries for OLED Display
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#include "DHT.h"
+
+#define DHTPIN 25
+//our sensor is DHT11 type
+#define DHTTYPE DHT11
+//create an instance of DHT sensor
+DHT dht(DHTPIN, DHTTYPE);
+
+//define the pins used by the LoRa transceiver module
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
+
+//433E6 for Asia
+//866E6 for Europe
+//915E6 for North America
+//920E6 for Japan
+#define BAND 920E6
+
+//OLED pins
+#define OLED_SDA 4
+#define OLED_SCL 15
+#define OLED_RST 16
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+//packet counter
+int counter = 0;
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+
+void setup() {
+
+  //reset OLED display via software
+  pinMode(OLED_RST, OUTPUT);
+  digitalWrite(OLED_RST, LOW);
+  delay(20);
+  digitalWrite(OLED_RST, HIGH);
+
+  //initialize OLED
+  Wire.begin(OLED_SDA, OLED_SCL);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;); // Don't proceed, loop forever
+  }
+
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.print("LORA SENDER ");
+  display.display();
+
+  //initialize Serial Monitor
+  Serial.begin(115200);
+
+  Serial.println("LoRa Sender Test");
+
+  //SPI LoRa pins
+  SPI.begin(SCK, MISO, MOSI, SS);
+  //setup LoRa transceiver module
+  LoRa.setPins(SS, RST, DIO0);
+
+  if (!LoRa.begin(BAND)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  Serial.println("LoRa Initializing OK!");
+  display.setCursor(0, 10);
+  display.print("LoRa Initializing OK!");
+  display.display();
+  delay(2000);
+  dht.begin();
+}
+
+void loop() {
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+
+  //Send LoRa packet to receiver
+  LoRa.beginPacket();
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("LORA SENDER");
+  display.setCursor(0, 20);
+  display.setTextSize(1);
+  display.print("LoRa packet sent.");
+  display.setCursor(0, 30);
+  display.print("Counter:");
+  display.setCursor(50, 30);
+  display.println(counter);
+  display.print("H:");
+  display.println(h);
+  display.print("T:");
+  display.println(t);
+  display.display();
+
+  counter++;
+
+  delay(10000);
+}
