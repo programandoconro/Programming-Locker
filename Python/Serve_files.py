9c6f7679b486e7d4dc3befa0bb01cfe92f64fbe9
Ro <50117686+programandoconro@users.noreply.github.com>
2020-12-14 19:48:01 +0900

    Create Serve_files.py

---
+++ Python/Serve_files.py
@@ -0,0 +1,7 @@
+#/usr/bin/python3
+
+#serve files in current directory on port 8000.
+
+python3 -m http.server
+
+# You can add an alias to .bashrc to access this feature more easily. echo alias serve='python3 -m http.server' >> ~/.bashrc && source ~/.bashrc
