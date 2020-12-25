6e2f755d74ad717f5c0e9f9c86bbe5cbf265ff31
Ro <50117686+programandoconro@users.noreply.github.com>
2020-12-25 13:13:20 +0900

    Create Binary_search.py

---
+++ Languages/Python/Binary_search.py
@@ -0,0 +1,39 @@
+import random
+from math import floor
+
+def binary_search(l, value):
+    l.sort()
+    cl = l
+
+    if value > l[len(l) - 1] or value < l[0]:
+        print("value not in the list")
+
+    else:
+
+        while True:
+            index =  floor((len(cl) / 2) - 1)
+            
+            if value > l[index]:
+                
+                cl = cl[floor((len(cl) / 2) - 1): len(cl)]
+                print("Value higher than")
+
+            if value < l[index]:
+                
+                cl = cl[0:floor((len(cl) / 2) - 1)]
+                print("Value lower than")
+
+            if value == l[index]:
+                answer = l[index]
+                print("Founded: ", answer)
+                break
+
+list_n = int(input("Please input the size of the array to search in: "))
+user_value = int(input("Please input a number to search: "))
+
+my_long_list = random.sample(range(list_n), list_n)
+
+binary_search(my_long_list, user_value)
+
+
+
