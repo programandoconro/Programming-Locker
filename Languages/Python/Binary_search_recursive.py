b6dcb179cb832fc3b3a1c75827e57cbc55fbc6c4
Ro <50117686+programandoconro@users.noreply.github.com>
2020-12-25 15:46:39 +0900

    Update Binary_search_recursive.py

--- Languages/Python/Binary_search_recursive.py
+++ Languages/Python/Binary_search_recursive.py
@@ -1,35 +1,37 @@
+#!/usr/bin/python3
+
 import random
 from math import floor
 
 my_long_list = random.sample(range(1000000), 1000000)
+
 def binary_search(l, value):
     l.sort()
-    cl = l
 
     def recursive(cl):
         
         index =  cl[floor(len(cl) / 2)]
-        if value > l[index]:
+        if value == l[index]:
+            answer = l[index]
+            print("Founded: ", answer)
+
+        elif value > l[index]:
             
             cl = cl[floor(len(cl) /2): len(cl)]
             recursive(cl)
             print("Value higher than")
 
-        if value < l[index]:
+        elif value < l[index]:
             
             cl = cl[0:floor(len(cl) / 2)]
             recursive(cl)
             print("Value lower than")
 
-        if value == l[index]:
-            answer = l[index]
-            print("Founded: ", answer)
-
-        if value > l[len(l) - 1] or value < l[0]:
-            print("value not in the list")
-        
     
-    recursive(cl)
+    if value > l[len(l) - 1] or value < l[0]:
+        print("value not in the list")
+    else:
+        recursive(l)
 
 binary_search(my_long_list, 33333)
 binary_search(my_long_list, 555555)
