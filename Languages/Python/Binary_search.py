9365cb4dd80ebfdd6f5c22083d7c0b20a94890db
Ro <50117686+programandoconro@users.noreply.github.com>
2020-12-25 15:56:11 +0900

    Update Binary_search.py

--- Languages/Python/Binary_search.py
+++ Languages/Python/Binary_search.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import random
 from math import floor
 
@@ -7,29 +9,27 @@ def binary_search(l, value):
     l.sort()
     cl = l
 
-    while True:
-        
-        index =  cl[floor(len(cl) / 2)]
-        if value > l[index]:
+    if value > l[len(l) - 1] or value < l[0]:
+        print("Value not in the list")
+    else:
+        while True:
+            index =  cl[floor(len(cl) / 2)]
+
+            if value == l[index]:
+                answer = l[index]
+                print("Found: ", answer)
+                break
             
-            cl = cl[floor(len(cl) /2): len(cl)]
-            print("Value higher than")
+            elif value > l[index]:
+                
+                cl = cl[floor(len(cl) /2): len(cl)]
+                print("Value higher than")
 
-        if value < l[index]:
-            
-            cl = cl[0:floor(len(cl) / 2)]
-            print("Value lower than")
+            elif value < l[index]:
+                
+                cl = cl[0:floor(len(cl) / 2)]
+                print("Value lower than")
 
-        if value == l[index]:
-            answer = l[index]
-            print("Founded: ", answer)
-            break
-
-        if value > l[len(l) - 1] or value < l[0]:
-            print("value not in the list")
-            break
-
-    
 
 binary_search(my_long_list, 33333)
 binary_search(my_long_list, 555555)
