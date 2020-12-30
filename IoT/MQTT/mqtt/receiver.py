1630d4ace458a7d04e6cf1a68644f20423cfce9b
Ro <50117686+programandoconro@users.noreply.github.com>
2020-12-30 23:45:22 +0900

    Update receiver.py

--- IoT/MQTT/mqtt/receiver.py
+++ IoT/MQTT/mqtt/receiver.py
@@ -1,27 +1,24 @@
-import paho.mqtt.client as mqtt
+import paho.mqtt.client as paho
 
-# The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("mytopic/test/example")
+    client.subscribe("test")
 
-# The callback for when a PUBLISH message is received from the server.
+f = open("messages.log","ab")
+def write_file(msj):
+    f.write(msj)
+    new_line = "\n"
+    f.write(new_line.encode("utf-8"))
+    
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-    with open('data.txt', 'wb') as f:
-        f.write(msg.payload)
+    print(msg.topic+"/"+str(msg.payload))
+    write_file(msg.payload)
 
-client = mqtt.Client()
+client = paho.Client()
+client.username_pw_set(username="name",password="xxxxxxx")
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("192.168.x.xxx", 1883, 60)
-
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
+client.connect("your broker ip", 1883, 60)
 client.loop_forever()
