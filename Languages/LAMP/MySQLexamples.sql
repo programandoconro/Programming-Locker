ad32d6849fb38cfb975257456277e3ed0a247666
Ro <50117686+programandoconro@users.noreply.github.com>
2021-01-01 16:10:22 +0900

    Update MySQLexamples.sql

--- Languages/LAMP/MySQLexamples.sql
+++ Languages/LAMP/MySQLexamples.sql
@@ -10,6 +10,11 @@ select * from data;
 insert into data values (1,7,0,0,'2020/03/04');
 delete from data where Id = 0;
 
+# Usuarios
+
+CREATE USER 'test' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON test.* TO 'test';
+
 #### Examples #####################
 
 select round(sum(LAT_N),2), round( sum(LONG_W),2) from STATION;
@@ -17,3 +22,5 @@ SELECT ROUND (MAX(LAT_N), 4) FROM STATION WHERE LAT_N < 137.2345;
 SELECT ROUND (LONG_W, 4) FROM STATION WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATION WHERE LAT_N < 137.2345);
 SELECT ROUND (MIN(LAT_N),4)FROM STATION WHERE LAT_N > 38.7780;
 select round(abs(min(lat_n)- max(lat_n)) + abs(min(long_w)- max(long_w)),4) from  station;
+
+                                               
