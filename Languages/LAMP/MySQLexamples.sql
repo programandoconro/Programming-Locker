5c721787fd5a37899500da807d2af6f2a908be06
Ro <50117686+programandoconro@users.noreply.github.com>
2021-01-04 18:05:16 +0900

    Update MySQLexamples.sql

--- Languages/LAMP/MySQLexamples.sql
+++ Languages/LAMP/MySQLexamples.sql
@@ -1,3 +1,4 @@
+SYSTEM echo "Hello world SQL"; 
 
 CREATE DATABASE test;
 SHOW DATABASES;
@@ -15,10 +16,10 @@ DELETE FROM TABLE data;
 DROP TABLE data;
 ALTER TABLE data DROP COLUMN Fecha;
 
-CREATE USER 'test' IDENTIFIED BY 'password';
+CREATE USER 'user01' IDENTIFIED BY 'password12345';
 GRANT ALL PRIVILEGES ON test.* TO 'test';
 
-#### Examples #####################
+#### Solutions to online tests #####################
 
 SELECT ROUND(SUM(LAT_N),2), ROUND(SUM(LONG_W),2) FROM STATION;
 SELECT ROUND (MAX(LAT_N), 4) FROM STATION WHERE LAT_N < 137.2345;
