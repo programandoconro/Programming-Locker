5741d922a3a426bb377d5e7c4d5fbbc4f6ec7dc8
Ro <50117686+programandoconro@users.noreply.github.com>
2021-01-04 17:51:18 +0900

    Update MySQLexamples.sql

--- Languages/LAMP/MySQLexamples.sql
+++ Languages/LAMP/MySQLexamples.sql
@@ -1,26 +1,29 @@
-## creacion de tabla dentro de database.
 
-create database test;
-show databases;
-use test;
-create table data (Id int, Contagiados int, Recuperados int, Muertos int);
-insert into data values (0,0,0,0);
-alter table data add (Fecha date);
-select * from data; 
-insert into data values (1,7,0,0,'2020/03/04');
-delete from data where Id = 0;
+CREATE DATABASE test;
+SHOW DATABASES;
+USE test;
+CREATE TABLE data (Id int, Contagiados int, Recuperados int, Muertos int);
 
-# Usuarios
+INSERT INTO data values (0,0,0,0);
+
+ALTER TABLE data ADD (Fecha date);
+SELECT * FROM data; 
+INSERT INTO data VALUES (1,7,0,0,'2020/03/04');
+
+DELETE FROM data WHERE Id = 0;
+DELETE FROM TABLE data;
+DROP TABLE data;
+ALTER TABLE data DROP COLUMN Fecha;
 
 CREATE USER 'test' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON test.* TO 'test';
 
 #### Examples #####################
 
-select round(sum(LAT_N),2), round( sum(LONG_W),2) from STATION;
+SELECT ROUND(SUM(LAT_N),2), ROUND(SUM(LONG_W),2) FROM STATION;
 SELECT ROUND (MAX(LAT_N), 4) FROM STATION WHERE LAT_N < 137.2345;
 SELECT ROUND (LONG_W, 4) FROM STATION WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATION WHERE LAT_N < 137.2345);
 SELECT ROUND (MIN(LAT_N),4)FROM STATION WHERE LAT_N > 38.7780;
-select round(abs(min(lat_n)- max(lat_n)) + abs(min(long_w)- max(long_w)),4) from  station;
+SELECT ROUND(ABS(MIN(lat_n) - MAX(lat_n)) + ABS(MIN(long_w)- MAX(long_w)),4) FROM STATION;
 
                                                
