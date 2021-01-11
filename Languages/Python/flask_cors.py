222a2951185c1fb1c5a4d1d42ab114c454b5e029
Ro <50117686+programandoconro@users.noreply.github.com>
2021-01-11 20:47:57 +0900

    Update flask_cors.py

--- Languages/Python/flask_cors.py
+++ Languages/Python/flask_cors.py
@@ -2,16 +2,25 @@
 
 import flask
 from flask import request, jsonify
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
-@app.route('/',methods=['GET'])
+
+app = flask.Flask( __name__ )
+app.config[ 'DEBUG' ] = True
+
+@app.route( '/', methods = ['GET'] )
+
 def home():
         return '''<h1> Test </h1> '''
-@app.route('/api/v1/data', methods= ['GET'])
+
+@app.route( '/api/v1/data', methods= ['GET'] )
+
 def api_all():
-        f = open('data.txt', 'r')
-        data = {'id': f.read()}
-        response = jsonify(data)
-        response.headers.add("Access-Control-Allow-Origin","*")
+        
+        f = open( 'data.txt', 'r' )
+        data = { 'id': f.read() }
+        response = jsonify( data )
+        response.headers.add( 'Access-Control-Allow-Origin', '*' )
+        
         return response
-app.run(host='0.0.0.0', port=8888)
+
+app.run( host='0.0.0.0', port=8888 )
+
