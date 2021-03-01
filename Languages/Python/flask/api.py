7c47f3af13cde30a7bafae36820ba48913d5601a
Ro <50117686+programandoconro@users.noreply.github.com>
2021-03-01 17:42:57 +0900

    Update api.py

--- Languages/Python/flask/api.py
+++ Languages/Python/flask/api.py
@@ -1,6 +1,15 @@
+import flask
 import requests
-api = 'https://api.github.com/users/jbperezf'
-r = requests.get(api)
-print (r.json())
 
+api = "https://api.github.com/users/programandoconro/repos"
+
+app = flask.Flask(__name__)
+
+def home():
+  r = requests.get(api)
+  results = r.json()
+  
+  return str(results)
+
+app.run()
 
