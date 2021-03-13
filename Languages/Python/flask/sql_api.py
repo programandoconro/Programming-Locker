f00b221d17aa1e99734aef206d6f74081f712fe5
Ro <50117686+programandoconro@users.noreply.github.com>
2021-03-13 16:05:39 +0900

    Updated sql_api flask

--- Languages/Python/flask/sql_api.py
+++ Languages/Python/flask/sql_api.py
@@ -1,32 +1,36 @@
 from mysql.connector import connect,Error
 import flask
-from flask import request, jsonify
+from flask import request, jsonify,render_template
+from collections import OrderedDict
 
 show = "show tables"
 select = "select * from Weather"
-data = {}
 
-try:
-    with connect(
-        host="192.168.1.166",
-        user="user",
-        password="*******",
-        database="Sensors"
-
-
-    ) as connection:
-        print(connection)
-        print("Success")
-        with connection.cursor() as cursor:
-            cursor.execute(show)
-            for db in cursor:
-                print(db)
-            cursor.execute(select)
-            for i,db in enumerate(cursor):
-                data[i] = db
-
-except Error as e:
-    print(e)
+def get_data():
+    data = OrderedDict()
+    try:
+        with connect(
+            host="192.168.1.166",
+            user="user",
+            password="***********",
+            database="Sensors"
+            
+        ) as connection:
+              print(connection)
+              print("Success")
+              with connection.cursor() as cursor:
+                  cursor.execute(show)
+                  for db in cursor:
+                      print(db)
+                  cursor.execute(select)
+                  for i,db in enumerate(cursor):
+                      data[i] = db
+                  for k,j in enumerate(reversed(data)):
+                      data[k] = data[j]
+    except Error as e:
+        print(e)
+    
+    return data
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -37,8 +41,16 @@ def home():
 
 @app.route("/api/temp&hum")
 def temp_hum():
-    response = jsonify(data)
+    response = jsonify(get_data())
     response.headers.add("Access-Control-Allow-Origin","*")
     return response
 
-app.run(host="0.0.0.0",port=5000)
+@app.route("/home")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000)
+
+    
+    
