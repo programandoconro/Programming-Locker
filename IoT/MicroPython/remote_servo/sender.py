0f1bab33153c0021555700a76c5b6f28611d257b
Ro <50117686+programandoconro@users.noreply.github.com>
2021-03-14 15:57:36 +0900

    Added AWS Mosquitto Broker

--- IoT/MicroPython/remote_servo/sender.py
+++ IoT/MicroPython/remote_servo/sender.py
@@ -1,58 +1,69 @@
+"""
+Send Temperature and orientation data from mpu6050 sensor to local and AWS MQTT Brokers.
+Using ESP32 chip and MicroPython.
+"""
+
 import mpu6050
 from machine import SoftI2C, Pin, PWM, ADC, reset
 from time import sleep
 import network
-import wifi
+import mqtt
 import math
+
 
 from umqtt import simple
 
+mqtt.wifi_connect()
+try:
+    mqtt.wifi_connect()
+except:
+    mqtt.hotspot_connect()
 
-def wifi_connect():
-    
-    ssid = 'Mi-Ro-Sa-Network'
-    wp2_pass = '*************'
-    sta_if = network.WLAN(network.STA_IF)
-    sta_if.active(True)
-    sta_if.connect(ssid, wp2_pass)
-    while sta_if.isconnected() == False:
-        pass
-    print(sta_if.isconnected())
+mqtt_server = "192.168.1.166"
+mqtt_aws = "roro.world"
 
-wifi_connect()
+client_id = "usero"
 
-mqtt_server = b"192.168.1.166"
-
-client_id = b"usero"
-
-topic_sub = b'temp_orientation'
-topic_pub = b'hello'
+topic_sub = 'temp_orientation'
 
 last_message = 0
 message_interval = 5
 counter = 0
 
 def sub_cb(topic, msg):
-  print((topic, msg))
-
+  print(topic, msg)
 
 def connect_and_subscribe():
   global client_id, mqtt_server, topic_sub
-  client = simple.MQTTClient(client_id, mqtt_server,user=b'user00001', password=b'***********')
+  client = simple.MQTTClient(client_id, mqtt_server,user='user00001', password='*********')
   client.set_callback(sub_cb)
   client.connect()
   client.subscribe(topic_sub)
   return client
 
+def connect_and_subscribe_aws():
+  global client_id, topic_sub,mqtt_aws
+  client_aws = simple.MQTTClient(client_id, mqtt_aws,user="user00001", password="*********")
+  client_aws.set_callback(sub_cb)
+  client_aws.connect()
+  client_aws.subscribe(topic_sub)
+  
+  return client_aws
+
 def restart_and_reconnect():
-  print(b'Failed to connect to MQTT broker. Reconnecting...')
+  print('Failed to connect to MQTT broker. Reconnecting...')
   sleep(1)
   reset()
 
 try:
-  client = connect_and_subscribe()
+  client_aws = connect_and_subscribe_aws()
 except OSError as e:
   restart_and_reconnect()
+
+try:
+  client = connect_and_subscribe()
+except OSError as e:
+  restart_and_reconnect(e)
 
 # mpu sensor
 
@@ -61,12 +72,13 @@ a = mpu6050.accel(i2c)
 
 tprev = 0
 mprev = False
+m = 0
 
 while True:
     
     values = a.get_values()
     z = values["AcZ"]
-    t = values["Tmp"]
+    t = math.floor(int(values["Tmp"]) * 10) / 10
     
     if int(z) < 0:
         m = "DOWN"
@@ -79,12 +91,29 @@ while True:
         tprev = t
         try:
             client.check_msg()
-            client.publish(b"temp_orientation", b"{} {}".format(str(math.floor(int(t*100))/100.0),m))
+            client.publish("temp_orientation", "{} {}".format(str(t),m))
         
         except OSError as e:
             restart_and_reconnect()
             
+        try:
+            client_aws.check_msg()
+            client_aws.publish("temp_orientation", "{} {}".format(str(t),m))
+        
+        except OSError as e:
+            restart_and_reconnect()        
+            
     sleep(0.2)
+    
+    
+    
         
         
+
+
+
+
+
+
+
 
