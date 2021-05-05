747fa0d89be34705f0271344215df181756120e4
Ro <50117686+programandoconro@users.noreply.github.com>
2021-05-05 15:13:57 +0900

    Get items from AWS DynamoDB

--- IoT/AWS/dynamo.py
+++ IoT/AWS/dynamo.py
@@ -2,9 +2,18 @@
 
 import boto3
 
+# Get item: 
 client = boto3.client("dynamodb")
-
 response = client.get_item(TableName='testo', Key={'documents':{'S':str('hola')}})
 
 print(response)
+
+# Get all items: 
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('testo')
+items = table.scan()['Items']
+
+for item in items:
+    print (item)
 
