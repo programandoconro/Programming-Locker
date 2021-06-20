48c379a0f834d025e0e757445f077d5833d10d75
Ro <50117686+programandoconro@users.noreply.github.com>
2021-06-20 11:11:54 +0900

    Update hakataminami.sh

--- IoT/SHINKANSEN/hakataminami.sh
+++ IoT/SHINKANSEN/hakataminami.sh
@@ -1,2 +1,6 @@
-curl "https://www.navitime.co.jp/diagram/depArrTimeList?departure=00007421&arrival=00007420&line=00000089&updown=0"| grep "time dep" | awk '{print substr($3,6,5)}' > schedule.txt
+# Get today's schedule for Shinkansen trains from Hakata-minami to Hakata station.
+
+curl "https://www.navitime.co.jp/diagram/depArrTimeList?departure=00007421&arrival=00007420&line=00000089&updown=0" |
+grep "time dep" |
+awk '{print substr($3,6,5)}'
 
