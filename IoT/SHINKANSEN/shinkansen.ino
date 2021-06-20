42329ee6f274b5cd807349f67b49464eadeae793
Ro <50117686+programandoconro@users.noreply.github.com>
2021-06-20 12:44:53 +0900

    Display time until next 3 Shinkansen
    Improved display layout and API updated

--- IoT/SHINKANSEN/shinkansen.ino
+++ IoT/SHINKANSEN/shinkansen.ino
@@ -1,4 +1,6 @@
-// Get the next Shinkansen train from Hakata-Minami to Hakata Station.
+/* Get and display time until the next 3 Shinkansen trains from Hakata-Minami to Hakata Station.
+   @programandoconro 2021
+*/
 
 #include <heltec.h>
 #include <WiFi.h>
@@ -24,7 +26,7 @@ void setup() {
 
 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(3000);
     Serial.println("Connecting to WiFi..");
   }
 
@@ -41,21 +43,30 @@ void loop() {
   if ((WiFi.status() == WL_CONNECTED)) { 
     HTTPClient http;
  
-    http.begin("http://192.168.1.228:9999/"); 
-    
+    http.begin("http://192.168.1.228:9999/hakataminami-hakata"); 
+    delay(10000);
     int httpCode = http.GET(); 
     delay(10000);
     if (httpCode > 0) { 
         Heltec.display->clear();
         
         String payload = http.getString();
+        
         Serial.println(httpCode);
         Serial.println(payload);
+
+        Heltec.display->setFont(ArialMT_Plain_10);
+
+        Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+        Heltec.display->drawString(0, 0,  "Next in: ");
+        Heltec.display->drawString(110, 50,  "H:M ");
         
         Heltec.display->setFont(ArialMT_Plain_24);
-        Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
-        Heltec.display->drawString(0, 0, payload);
 
+        Heltec.display->drawString(50, 0,  payload.substring(2,6));
+        Heltec.display->drawString(50, 20, payload.substring(12,16));
+        Heltec.display->drawString(50, 40, payload.substring(22,26));
+        
         Heltec.display->display();
         
       }
