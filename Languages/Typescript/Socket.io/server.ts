1a18db98699f16f6e851f865183d7cb0a960bfea
Rodrigo Díaz Lupanow <programandoconro@gmail.com>
2021-09-24 23:05:20 +0900

    Simple chat socket.io + react + ts

--- Languages/Typescript/Socket.io/server.ts
+++ Languages/Typescript/Socket.io/server.ts
@@ -1,10 +1,19 @@
 import express from "express";
 import { Server } from "socket.io";
 import http from "http";
+import cors from "cors";
+import helmet from "helmet";
 
 const App: express.Express = express();
+App.use(cors());
+App.use(helmet());
 const httpServer = new http.Server(App);
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 let nu: number = 0;
 App.get("/", (_, res) => {
@@ -12,12 +21,13 @@ App.get("/", (_, res) => {
 });
 io.on("connect", (socket) => {
   console.log(`connect ${socket.id}`);
-
-  socket.on("ping", () => {
-    console.log(nu);
-    nu = nu + 1;
+  socket.on("message", (data) => {
+    console.log("ping", data);
+    socket.broadcast.emit("message", {
+      user: data.user,
+      message: data.message,
+    });
   });
-
   socket.on("disconnect", () => {
     console.log(`disconnect ${socket.id}`);
   });
