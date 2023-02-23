63d08a8c41e1c2adb336f9353b84090567c3470c
Ro <50117686+programandoconro@users.noreply.github.com>
2023-02-23 22:49:25 +0900

    Update flip-board.rs

--- Languages/Rust/yew/chessboard/flip-board.rs
+++ Languages/Rust/yew/chessboard/flip-board.rs
@@ -4,31 +4,21 @@ type Board = Vec<Vec<String>>;
 
 fn create_board(flipped: bool) -> Board {
     let mut board: Board = Vec::new();
-    let columns: Vec<char> = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-    let rows: Vec<char> = vec!['1', '2', '3', '4', '5', '6', '7', '8'];
+    let mut columns: Vec<char> = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+    let mut rows: Vec<char> = vec!['1', '2', '3', '4', '5', '6', '7', '8'];
+
     if flipped {
-        for row in rows {
-            board.push(Vec::new());
-            for column in &columns {
-                let mut square = String::new();
-                square.push(*column);
-                square.push(row);
-                let len: usize = board.len();
-                board[len - 1].push(square);
-                //web_sys::console::log_1(&square.into());
-            }
-        }
-    } else {
-        let columns_reversed: Vec<char> = columns.into_iter().rev().collect();
-        for row in rows.into_iter().rev() {
-            board.push(Vec::new());
-            for column in &columns_reversed {
-                let mut square = String::new();
-                square.push(*column);
-                square.push(row);
-                let len: usize = board.len();
-                board[len - 1].push(square);
-            }
+        columns = columns.into_iter().rev().collect();
+        rows = rows.into_iter().rev().collect();
+    }
+    for row in rows {
+        board.push(Vec::new());
+        for column in &columns {
+            let mut square = String::new();
+            square.push(*column);
+            square.push(row);
+            let len: usize = board.len();
+            board[len - 1].push(square);
         }
     }
 
