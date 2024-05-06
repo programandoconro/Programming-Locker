57d4130195c42572b6fdc8c67946ce4dcdba9028
Ro <50117686+programandoconro@users.noreply.github.com>
2024-05-06 11:13:07 +0900

    Update init.lua

--- OS/Linux/Dotfiles/nvim/init.lua
+++ OS/Linux/Dotfiles/nvim/init.lua
@@ -1,9 +1,52 @@
 require('plugins')
 require('coc')
-require('remaps')
-require('mason').setup()
-require('lsp')
+--require('nerdtree')
+--require("telescope").load_extension("recent_files")
+require("remaps")
+--require("mason").setup()
+--require("lsp")
+require("toggleterm").setup{}
+
+
+vim.cmd[[colorscheme dracula]]
 
 vim.cmd('source ~/.config/nvim/config.vim')
+
+vim.wo.relativenumber = true
+vim.opt.number = true
+vim.opt.signcolumn = "number"
+
+
+-- Nvim tree
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+
+
+
+
 
 
